466ceeffce6251a33ee6dfacda62a54f030db262
LuminarHub <sivapriya@luminartechnolab.com>
2024-05-01 15:17:47 +0530

    ert

--- Backend/ParkingSpaceFinder/api/admin.py
+++ Backend/ParkingSpaceFinder/api/admin.py
@@ -6,3 +6,6 @@ from .models import *
 admin.site.register(ReservedSlots)
 admin.site.register(ReservedZones)
 admin.site.register(ParkZone)
+admin.site.register(State)
+admin.site.register(District)
+admin.site.register(Location)
